5f7a37755164ec6f64996a58d2b92dcde3824017
Santhosh Gutta <Santhosh.Gutta@sanrus.ca>
2018-10-14 12:38:30 -0400

    Added demo source code for data types, variables, scope & lifetime and conversion.

---
+++ DataTypesAndVariables/src/DataTypesAndVariablesDemo.java
@@ -0,0 +1,64 @@
+
+public class DataTypesAndVariablesDemo {
+
+	public static void main(String[] args) {
+		//declaration of a variable
+		byte b1;
+		
+		//initialization of b1 variable
+		b1 = -128;
+		
+		short s = 32767;
+		
+		int i = 565644677;
+		
+		byte i1 = 25;
+		
+		long l12 = 9813792739279327l;
+		
+		String s1 = "ABCD";
+		
+		boolean result = true;
+		
+		char ch = '\u00B0';
+		char hindiCharacter = '\u0905';
+		
+		
+		float f = 3.1412345678909876543F;
+		double d = 3.1412345678909876543; 
+		
+		
+		System.out.println("byte b1 => " + b1);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l12);
+		System.out.println(s1);
+		System.out.println(result);
+		System.out.println("30" + ch + " C");
+		System.out.println(hindiCharacter);
+		System.out.println(f);
+		System.out.println(d);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
